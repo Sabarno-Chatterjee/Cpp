2939821a01bf41531c787f7b3efd82476a183fce
Sabarno Chatterjee <sabarno.05@gmail.com>
2023-09-21 20:43:07 +0530

    edit

--- practice.cpp
+++ practice.cpp
@@ -1,33 +1,62 @@
+//Write a C++ program to check whether a number is Armstrong number or not.
+
+// #include<iostream>
+// #include<math.h>
+
+// using namespace std;
+
+// void armstrong(int num){
+//     int original_num=num, sum=0, count=0;
+//     while(num){
+//         count++;
+//         num/=10;
+//     }
+    
+//     num=original_num;
+    
+//     while(num){
+//         sum+= pow(num%10, count);
+//         num /= 10;
+//     }
+//     if(original_num==sum)
+//         cout<<"Armstrong."<<endl;
+//     else
+//         cout<<"Not armstrong."<<endl;
+    
+// }
+
+// int main(){
+//     int num;
+//     cout<<"Enter a number.\n"<<endl;
+//     cin>>num;
+//     armstrong(num);
+    
+//     return 0;
+// }
+
+// Can you convert a decimal number to binary?
+
+
 #include<iostream>
-#include<math.h>
 
 using namespace std;
 
-void armstrong(int num){
-    int original_num=num, sum=0, count=0;
+void to_binary(int num){
+    int count =0, arr[100];
     while(num){
+        arr[count] = num%2;
         count++;
-        num/=10;
+        num/=2;
     }
-    
-    num=original_num;
-    
-    while(num){
-        sum+= pow(num%10, count);
-        num /= 10;
-    }
-    if(original_num==sum)
-        cout<<"Armstrong."<<endl;
-    else
-        cout<<"Not armstrong."<<endl;
-    
+    for(int i =count-1; i>=0;i--)
+        cout<<arr[i];
 }
 
 int main(){
     int num;
-    cout<<"Enter a number.\n"<<endl;
+    cout<<"Enter a number"<<endl;
     cin>>num;
-    armstrong(num);
+    to_binary(num);
     
     return 0;
 }
