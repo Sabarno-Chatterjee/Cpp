33fdf9f3aba216d873db66c770559ae0a136f973
Sabarno Chatterjee <sabarno.05@gmail.com>
2023-09-22 17:57:57 +0530

    edit

--- practice.cpp
+++ practice.cpp
@@ -37,26 +37,54 @@
 // Can you convert a decimal number to binary?
 
 
+// #include<iostream>
+
+// using namespace std;
+
+// void to_binary(int num){
+//     int count =0, arr[100];
+//     while(num){
+//         arr[count] = num%2;
+//         count++;
+//         num/=2;
+//     }
+//     for(int i =count-1; i>=0;i--)
+//         cout<<arr[i];
+// }
+
+// int main(){
+//     int num;
+//     cout<<"Enter a number"<<endl;
+//     cin>>num;
+//     to_binary(num);
+    
+//     return 0;
+// }
+
+
+//Now can you revert back the original number from binary? Convert Binary to Decimal number.
+
 #include<iostream>
+#include<math.h>
 
 using namespace std;
 
-void to_binary(int num){
-    int count =0, arr[100];
+void binary_to_decimal(int num){
+    int decimal=0, i=0;
     while(num){
-        arr[count] = num%2;
-        count++;
-        num/=2;
+        decimal += num%10 * pow(2, i);
+        num /= 10;
+        i++;
     }
-    for(int i =count-1; i>=0;i--)
-        cout<<arr[i];
+    cout<<decimal<<endl;
 }
 
 int main(){
     int num;
-    cout<<"Enter a number"<<endl;
+    cout<<"Enter a binary number."<<endl;
     cin>>num;
-    to_binary(num);
+    binary_to_decimal(num);
     
     return 0;
 }
+
