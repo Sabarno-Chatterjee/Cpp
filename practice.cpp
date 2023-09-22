f8eceede48cc96ce0e786b024683dc30d777d855
Sabarno Chatterjee <sabarno.05@gmail.com>
2023-09-22 18:14:41 +0530

    edit

--- practice.cpp
+++ practice.cpp
@@ -92,27 +92,51 @@
 
 //Find the maximum number in an array of integers.
 
+// #include<iostream>
+
+// using namespace std;
+
+// int max(int arr[], int length){
+//     int temp=0;
+//     for(int i=0; i<length-1; i++)
+//         for(int j=0; j<length-1-i; j++){
+//             if(arr[j]>arr[j+1]){
+//                 temp=arr[j];
+//                 arr[j]=arr[j+1];
+//                 arr[j+1]= temp;
+//             }
+//         }
+//     return arr[length-1];
+// }
+
+// int main(){
+//     int arr[]={103,56,34,12,102,43,23,78,54,32,43,189};
+//     int length= sizeof(arr)/sizeof(arr[0]);
+//     cout<<"Max: "<<max(arr, length)<<endl;
+    
+//     return 0;
+// }
+
+//Can you find out the sum of the digits of a number?
+
 #include<iostream>
 
 using namespace std;
 
-int max(int arr[], int length){
-    int temp=0;
-    for(int i=0; i<length-1; i++)
-        for(int j=0; j<length-1-i; j++){
-            if(arr[j]>arr[j+1]){
-                temp=arr[j];
-                arr[j]=arr[j+1];
-                arr[j+1]= temp;
-            }
-        }
-    return arr[length-1];
+int sum_of_digits(int num){
+    int sum=0;
+    while(num){
+        sum+= num%10;
+        num/=10;
+    }
+    return sum;
 }
 
 int main(){
-    int arr[]={103,56,34,12,102,43,23,78,54,32,43,189};
-    int length= sizeof(arr)/sizeof(arr[0]);
-    cout<<"Max: "<<max(arr, length)<<endl;
+    int num;
+    cout<<"Enter a number"<<endl;
+    cin>>num;
+    cout<<"Sum: "<<sum_of_digits(num)<<endl;
     
     return 0;
 }
