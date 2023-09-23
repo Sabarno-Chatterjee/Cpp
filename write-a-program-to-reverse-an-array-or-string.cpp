3ea993c081cc298b14e39d0afcf307e0db6931e6
Sabarno Chatterjee <sabarno.05@gmail.com>
2023-09-23 15:05:58 +0530

    edit

--- write-a-program-to-reverse-an-array-or-string.cpp
+++ write-a-program-to-reverse-an-array-or-string.cpp
@@ -2,20 +2,61 @@
 
 //Using swap and iteration.
 
+// #include<iostream>
+
+// using namespace std;
+
+// void reverse_array(int arr[],int n){
+//     int start=0, end= n-1, temp=0;
+    
+//     while(start<end){
+//         temp = arr[start];
+//         arr[start] = arr[end];
+//         arr[end] = temp;
+//         start++;
+//         end--;
+//     }
+// }
+
+// void print_array(int arr[],int n){
+//     for(int i=0; i<n; i++)
+//         cout<<arr[i]<<" ";
+    
+//     cout<<endl;    
+// }
+
+// int main(){
+//     int arr[]={34,23,67,43,65,97,23,41,69};
+//     int length = sizeof(arr)/sizeof(arr[0]);
+    
+//     //Function to print array
+//     print_array(arr, length);
+    
+//     //Function to reverse the array
+//     reverse_array(arr, length);
+    
+//     cout<<"Reversed array: "<<endl;
+//     print_array(arr, length);
+    
+//     return 0;
+// }
+
+// Using Recursively call
+
 #include<iostream>
 
 using namespace std;
 
-void reverse_array(int arr[],int n){
-    int start=0, end= n-1, temp=0;
+void reverse_array(int arr[],int start,int end){
     
-    while(start<end){
-        temp = arr[start];
-        arr[start] = arr[end];
-        arr[end] = temp;
-        start++;
-        end--;
-    }
+    if(start>=end)
+        return;
+        
+    int temp = arr[start];
+    arr[start] = arr[end];
+    arr[end] = temp;
+    
+    reverse_array(arr,start+1,end-1);
 }
 
 void print_array(int arr[],int n){
@@ -33,7 +74,7 @@ int main(){
     print_array(arr, length);
     
     //Function to reverse the array
-    reverse_array(arr, length);
+    reverse_array(arr,0, length-1);
     
     cout<<"Reversed array: "<<endl;
     print_array(arr, length);
