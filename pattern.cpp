ded61894b558b2dac9c50105d10e45e61da7a563
Sabarno Chatterjee <sabarno.05@gmail.com>
2023-09-29 00:33:43 +0530

    pattern questions

--- pattern.cpp
+++ pattern.cpp
@@ -58,24 +58,52 @@
 //     return 0;
 // }
 
-#include<iostream>
+// #include<iostream>
 
-using namespace std;
+// using namespace std;
 
-void pattern(int n){
-    int j=1;
-    for(int i=1; i<=n;i++){
-        for(;j<=n*i;j++){
-            cout<<j<<" ";
-        }
+// void pattern(int n){
+//     int j=1;
+//     for(int i=1; i<=n;i++){
+//         for(;j<=n*i;j++){
+//             cout<<j<<" ";
+//         }
         
-    cout<<endl;     
-    }
+//     cout<<endl;     
+//     }
         
         
-}
+// }
 
 int main(){
     pattern(5);
     return 0;
 }
+
+hollow rectangle
+
+#include<iostream>
+
+using namespace std;
+
+void pattern(int n, int m){
+    
+    for(int i=1; i<=n;i++){
+        for(int j=1;j<=m;j++){
+            if(i==1 || i==n || j==1 || j==m)
+                cout<<"*";
+            else
+                cout<<" ";
+        }
+    cout<<endl; 
+    }       
+}
+        
+
+int main(){
+    pattern(7,9);
+    return 0;
+}
+
+
+
