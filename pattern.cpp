0c4e5d8ee287c59bf9e3e066a9d80a63bfc2e3cc
Sabarno Chatterjee <sabarno.05@gmail.com>
2023-09-29 08:49:25 +0530

    diamond pattern

--- pattern.cpp
+++ pattern.cpp
@@ -134,25 +134,62 @@
 
 // Star pyramid
 
+// #include<iostream>
+
+// using namespace std;
+
+// void pattern(int n){
+//     for(int i=1; i<=n; i++){
+//         for(int k=1; k<=n-i; k++){
+//             cout<<" ";
+//         }
+//         for(int j=1; j<=i;j++){
+//             cout<<"* ";
+//         }
+//     cout<<endl;
+//     }    
+// }
+
+// int main(){
+//     pattern(5);
+
+//     return 0;
+// }
+
+
+// Diamond pattern
+
 #include<iostream>
 
 using namespace std;
 
 void pattern(int n){
-    for(int i=1; i<=n; i++){
-        for(int k=1; k<=n-i; k++){
+    for(int i=1;i<=n;i++){
+        for(int k=1; k<=n-i;k++){
             cout<<" ";
         }
-        for(int j=1; j<=i;j++){
+        for(int j=1;j<=i;j++){
             cout<<"* ";
         }
+        cout<<endl;
+    }
+        
+    for(int i=n-1;i>=1;i--){
+        for(int k=1; k<=n-i;k++){
+            cout<<" ";
+        }
+        for(int j=i;j>=1;j--){
+            cout<<"* ";
+        }
+    
     cout<<endl;
-    }    
+    }
 }
 
 int main(){
-    pattern(5);
-
+    pattern(8);
+    
     return 0;
 }
+
 
