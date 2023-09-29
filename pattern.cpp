a9535010007e6b51349e612abada483b36335f97
Sabarno Chatterjee <sabarno.05@gmail.com>
2023-09-29 16:33:46 +0530

    k pattern

--- pattern.cpp
+++ pattern.cpp
@@ -384,24 +384,79 @@
 
 // zero-one triangle
 
+// #include<iostream>
+
+// using namespace std;
+
+// void pattern(int n){
+    
+//     for(int i=1; i<=n; i++){
+//         for(int j=1;j<=i;j++){
+//             if((i+j)%2==0)
+//                 cout<<"1"<<" ";
+//             else
+//                 cout<<"0"<<" ";
+//         }
+//     cout<<endl;
+//     }
+// }
+
+// int main(){
+//     pattern(8);
+//     return 0;
+// }
+
+
+// rhombus
+
+// #include<iostream>
+
+// using namespace std;
+
+// void pattern(int n){
+    
+//     for(int i=n; i>=1; i--){
+//         for(int k=1;k<=n-i;k++){
+//             cout<<" ";
+//         }
+//         for(int j=1;j<=n;j++){
+//             cout<<"* ";
+//         } 
+//     cout<<endl;
+//     }
+    
+// }
+
+// int main(){
+//     pattern(8);
+//     return 0;
+// }
+
+// k pattern
+
 #include<iostream>
 
 using namespace std;
 
 void pattern(int n){
     
-    for(int i=1; i<=n; i++){
+    for(int i=n; i>=1; i--){
+        
         for(int j=1;j<=i;j++){
-            if((i+j)%2==0)
-                cout<<"1"<<" ";
-            else
-                cout<<"0"<<" ";
-        }
+            cout<<"* ";
+        } 
     cout<<endl;
     }
+    for(int i=2; i<=n; i++){
+        for(int j=1;j<=i;j++){
+            cout<<"* ";
+        } 
+    cout<<endl;
+    }
+    
 }
 
 int main(){
-    pattern(8);
+    pattern(4);
     return 0;
 }
