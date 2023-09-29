843a9006b839f78fbe1062207953a82ddc69edc0
Sabarno Chatterjee <sabarno.05@gmail.com>
2023-09-30 00:10:30 +0530

    reverse left half pyramid

--- pattern.cpp
+++ pattern.cpp
@@ -464,35 +464,97 @@
 
 // mirror image triangle pattern
 
-#include<iostream>
+// #include<iostream>
 
+// using namespace std;
+
+// void pattern(int n){
+    
+//     for(int i=n; i>=1; i--){
+//         for(int k=1; k<=n-i; k++){
+//             cout<<" ";
+//         }
+        
+//         for(int j=1;j<=i;j++){
+//             cout<<"* ";
+//         } 
+//     cout<<endl;
+//     }
+//     for(int i=2; i<=n; i++){
+//         for(int k=1; k<=n-i; k++){
+//             cout<<" ";
+//         }
+//         for(int j=1;j<=i;j++){
+//             cout<<"* ";
+//         } 
+//     cout<<endl;
+//     }
+    
+// }
+
+// int main(){
+//     pattern(8);
+//     return 0;
+// }
+
+
+// pallindrome triangle
+
+// #include<iostream>
+// using namespace std;
+
+// void pattern(int n){
+//     int i, j;
+    
+//     // outer loop to handle the number of rows
+//     for(i = 1; i <= n; i++){
+        
+//         // inner loop to print spaces
+//         for(int k = 1; k <= 2 * (n - i); k++)
+//             cout << " ";
+        
+//         // inner loop to print the first part
+//         for(j = i; j >= 1; j--)
+//             cout << j << " ";
+        
+//         // inner loop to print the second part    
+//         for(j = 2; j <= i; j++)
+//             cout << j << " ";
+        
+//         // Move to the next line after each row
+//         cout << endl;
+//     }
+// }
+
+// int main(){
+//     pattern(4); 
+//     return 0;
+// }
+
+// Reverse left half pyramid
+
+#include<iostream>
 using namespace std;
 
 void pattern(int n){
-    
-    for(int i=n; i>=1; i--){
-        for(int k=1; k<=n-i; k++){
+    for(int i=n; i>=1;i--){
+        for(int k=1;k<=(n-i);k++){
             cout<<" ";
         }
         
         for(int j=1;j<=i;j++){
-            cout<<"* ";
-        } 
-    cout<<endl;
-    }
-    for(int i=2; i<=n; i++){
-        for(int k=1; k<=n-i; k++){
-            cout<<" ";
+            cout<<"*";
         }
-        for(int j=1;j<=i;j++){
-            cout<<"* ";
-        } 
-    cout<<endl;
+        cout<<endl;
     }
     
 }
 
 int main(){
-    pattern(8);
+    pattern(5); 
     return 0;
 }
+
+
+
+
