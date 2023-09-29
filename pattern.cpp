ff1df76ef8943dda950f2d814efb2076e5fd915c
Sabarno Chatterjee <sabarno.05@gmail.com>
2023-09-30 00:11:45 +0530

    reverse number triangle pattern

--- pattern.cpp
+++ pattern.cpp
@@ -533,25 +533,53 @@
 
 // Reverse left half pyramid
 
+// #include<iostream>
+// using namespace std;
+
+// void pattern(int n){
+//     for(int i=n; i>=1;i--){
+//         for(int k=1;k<=(n-i);k++){
+//             cout<<" ";
+//         }
+        
+//         for(int j=1;j<=i;j++){
+//             cout<<"*";
+//         }
+//         cout<<endl;
+//     }
+    
+// }
+
+// int main(){
+//     pattern(5); 
+//     return 0;
+// }
+
+
+// Reverse number Triangle Pattern
+
 #include<iostream>
 using namespace std;
 
 void pattern(int n){
-    for(int i=n; i>=1;i--){
-        for(int k=1;k<=(n-i);k++){
+    
+    for(int i=1; i<=n; i++){
+        
+        for(int k=1; k<i; k++){
             cout<<" ";
         }
         
-        for(int j=1;j<=i;j++){
-            cout<<"*";
+        for(int j=i;j<=n;j++){
+            cout<<j<<" ";
         }
+        
         cout<<endl;
+        
     }
-    
 }
 
 int main(){
-    pattern(5); 
+    pattern(4); 
     return 0;
 }
 
