b620e8773bd58ce02a3c6dad4e6cace96f8b9431
Sabarno Chatterjee <sabarno.05@gmail.com>
2023-09-29 09:34:08 +0530

    solid rhombus pattern

--- pattern.cpp
+++ pattern.cpp
@@ -159,31 +159,56 @@
 
 // Diamond pattern
 
+// #include<iostream>
+
+// using namespace std;
+
+// void pattern(int n){
+//     for(int i=1;i<=n;i++){
+//         for(int k=1; k<=n-i;k++){
+//             cout<<" ";
+//         }
+//         for(int j=1;j<=i;j++){
+//             cout<<"* ";
+//         }
+//         cout<<endl;
+//     }
+        
+//     for(int i=n-1;i>=1;i--){
+//         for(int k=1; k<=n-i;k++){
+//             cout<<" ";
+//         }
+//         for(int j=i;j>=1;j--){
+//             cout<<"* ";
+//         }
+    
+//     cout<<endl;
+//     }
+// }
+
+// int main(){
+//     pattern(8);
+    
+//     return 0;
+// }
+
+
+// Solid Rhombus
+
 #include<iostream>
 
 using namespace std;
 
 void pattern(int n){
-    for(int i=1;i<=n;i++){
-        for(int k=1; k<=n-i;k++){
+    for(int i=1; i<=n; i++){
+        for(int k=1; k<=n-i; k++)
             cout<<" ";
-        }
-        for(int j=1;j<=i;j++){
-            cout<<"* ";
-        }
+        for(int j=1; j<=n; j++)
+            cout<<"*";
+        
         cout<<endl;
     }
-        
-    for(int i=n-1;i>=1;i--){
-        for(int k=1; k<=n-i;k++){
-            cout<<" ";
-        }
-        for(int j=i;j>=1;j--){
-            cout<<"* ";
-        }
     
-    cout<<endl;
-    }
 }
 
 int main(){
@@ -191,5 +216,6 @@ int main(){
     
     return 0;
 }
+
 
 
