7cdd8bf273e6c0c23e4108a4a71cff96158cb5d6
Sabarno Chatterjee <sabarno.05@gmail.com>
2023-10-02 18:57:42 +0530

    edit

--- pattern.cpp
+++ pattern.cpp
@@ -732,6 +732,34 @@
 
 // pyramid
 
+// #include<iostream>
+// using namespace std;
+
+// void Draw(int n)
+// {
+    
+//     for(int i=1;i<=n;i++){
+//         for(int j=1;j<=n;j++){
+//             if(i+j>=n+1){
+//                 cout<<"* ";
+//             }
+//             else{
+//                 cout<<" ";
+//             }
+//         }
+//         cout<<endl;
+//     }
+// }
+
+
+// int main(){
+//     Draw(5);
+    
+//     return 0;
+// }
+
+// pattern reverse triangle
+
 #include<iostream>
 using namespace std;
 
@@ -740,7 +768,7 @@ void Draw(int n)
     
     for(int i=1;i<=n;i++){
         for(int j=1;j<=n;j++){
-            if(i+j>=n+1){
+            if(i+j<=n+1){
                 cout<<"* ";
             }
             else{
@@ -753,7 +781,7 @@ void Draw(int n)
 
 
 int main(){
-    Draw(5);
+    Draw(6);
     
     return 0;
 }
