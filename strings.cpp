cd521fc77e5573dc5d28b9ae28fa9bb19ae47bf4
Sabarno Chatterjee <sabarno.05@gmail.com>
2023-10-03 18:41:20 +0530

    lowercase to uppercase

--- strings.cpp
+++ strings.cpp
@@ -26,14 +26,39 @@
 
 // Convert uppercase string to lowercase
 
+// #include<iostream>
+// #include<cstring>
+
+// void str_lower(char str[]){
+//     int count=0;
+//     while(str[count]!='\0'){
+//         if(str[count]>=65 && str[count]<=90)
+//             str[count]+=32;
+//         count++;
+//     }
+//     std::cout<<str<<std::endl;
+// }
+
+// int main(){
+//     char str[50];
+//     std::cout<<"Enter a string to convert to lowercase"<<std::endl;
+//     std::cin.getline(str,50);
+//     str_lower(str);
+
+//     return 0;
+// }
+
+
+// Convert lowercase string to uppercase
+
 #include<iostream>
 #include<cstring>
 
 void str_lower(char str[]){
     int count=0;
     while(str[count]!='\0'){
-        if(str[count]>=65 && str[count]<=90)
-            str[count]+=32;
+        if(str[count]>=97 && str[count]<=122)
+            str[count]-=32;
         count++;
     }
     std::cout<<str<<std::endl;
@@ -41,7 +66,7 @@ void str_lower(char str[]){
 
 int main(){
     char str[50];
-    std::cout<<"Enter a string to convert to lowercase"<<std::endl;
+    std::cout<<"Enter a string to convert to uppercase"<<std::endl;
     std::cin.getline(str,50);
     str_lower(str);
 
