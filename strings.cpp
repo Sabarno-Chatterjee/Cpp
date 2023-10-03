b91dcb0e730e0b1ade66908ac1c50a765a9a93c9
Sabarno Chatterjee <sabarno.05@gmail.com>
2023-10-03 18:36:19 +0530

    uppercase to lowercase

--- strings.cpp
+++ strings.cpp
@@ -1,24 +1,49 @@
 // Find length of a string without using library functions
 
+// #include<iostream>
+// #include<cstring>
+
+// using namespace std;
+
+// int get_length(char str[]){
+//     int count=0;
+//     while(str[count]!='\0'){
+//             count++;
+//     }
+//     return count;
+// }
+
+// int main(){
+
+//     char str[50];
+//     cout<<"Enter a string"<<endl;
+//     cin.getline(str,50);
+//     cout<<get_length(str)<<endl;
+
+//     return 0;
+// }
+
+
+// Convert uppercase string to lowercase
+
 #include<iostream>
 #include<cstring>
 
-using namespace std;
-
-int get_length(char str[]){
+void str_lower(char str[]){
     int count=0;
     while(str[count]!='\0'){
-            count++;
+        if(str[count]>=65 && str[count]<=90)
+            str[count]+=32;
+        count++;
     }
-    return count;
+    std::cout<<str<<std::endl;
 }
 
 int main(){
-
     char str[50];
-    cout<<"Enter a string"<<endl;
-    cin.getline(str,50);
-    cout<<get_length(str)<<endl;
+    std::cout<<"Enter a string to convert to lowercase"<<std::endl;
+    std::cin.getline(str,50);
+    str_lower(str);
 
     return 0;
 }
