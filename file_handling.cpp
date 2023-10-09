fc1fdee136bf27edeaa80c62c4c2d389dc38257f
Sabarno Chatterjee <sabarno.05@gmail.com>
2023-10-09 19:54:49 +0530

    writing objects into a file

--- file_handling.cpp
+++ file_handling.cpp
@@ -53,29 +53,40 @@ class Student{
     string name;
     int roll;
     string branch;
+
+    //member functions
+    Student(){}
+    Student(string name, int roll, string branch){
+        this->name=name;
+        this->roll=roll;
+        this->branch=branch;
+    }
+
+    friend ofstream & operator<<(ofstream &outfile, Student &s);
 };
 
-int main(){
-    Student s;
-    s.name="Harry";
-    s.roll=1;
-    s.branch="CS";
-
-    ofstream outfile("new.txt", ios::trunc);
+ofstream & operator<<(ofstream &outfile, Student &s){
     outfile<<s.name<<endl;
     outfile<<s.roll<<endl;
     outfile<<s.branch<<endl;
-    
+
+    return outfile;
+}
+
+int main(){
+    Student s;
+    s.name="Ron";
+    s.roll=5;
+    s.branch="Potion";
+
+    ofstream outfile("new.txt", ios::trunc);
+    outfile<<s;
+
+    outfile.close();
+
     return 0;
 }
 
 
 
-    // public:
-    // //member functions
-    // Student(){}
-    // Student(string name, int roll, string branch){
-    //     this->name=name;
-    //     this->roll=roll;
-    //     this->branch=branch;
-    // }
+    
