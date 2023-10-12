85f698d45a0829a1904af172c66f36fe9a4a58ba
Sabarno Chatterjee <sabarno.05@gmail.com>
2023-10-12 21:23:35 +0530

    edit

--- object_oriented_programming_questions.cpp
+++ object_oriented_programming_questions.cpp
@@ -13,11 +13,34 @@ class Person{
 
     public:
     Person(string name="abc", int age=0, string country="xyz");
+
+    //Accessor methods
+    string getName(){return name;}
+    int getAge(){return age;}
+    string getCountry(){return country;}
+
+    //Mutator methods
+    void setName(string name);
+    void setAge(int age);
+    void setCountry(string country);
+
 };
 
 Person::Person(string name, int age, string country){
     this->name=name;
     this->age=age;
+    this->country=country;
+}
+
+void Person::setName(string name){
+    this->name=name;
+}
+
+void Person::setAge(int age){
+    this->age=age;
+}
+
+void Person::setCountry(string country){
     this->country=country;
 }
 
