1ad624e2a4943b8198aac384354585b69a8cfcf5
Sabarno Chatterjee <sabarno.05@gmail.com>
2023-10-12 23:26:51 +0530

    edit

--- object_oriented_programming_questions.cpp
+++ object_oriented_programming_questions.cpp
@@ -64,60 +64,80 @@
 
 /*Write a C++ program to create a class called Car that has private member variables for company, model, and year. Implement member functions to get and set these variables.*/
 
+// #include<iostream>
+
+// using namespace std;
+
+// class Car{
+//     //data members
+//     private:
+//     string company;
+//     string model;
+//     int year;
+
+//     //member functions
+//     public:
+//     Car(string company="abc", string model="xyz",int year=0);
+//     //accessor methods
+//     string getCompany(){return company;}
+//     string getModel(){return model;}
+//     int getYear(){return year;}
+
+//     //mutator methods
+//     void setCompany(string company);
+//     void setModel(string model);
+//     void setYear(int year);
+//     friend ostream & operator<<(ostream &out, Car &c);
+// };
+
+// int main(){
+//     Car c("Toyota","Innova",2009);
+//     cout<<c;
+//     return 0;
+// }
+
+// Car::Car(string company, string model,int year){
+//     this->company=company;
+//     this->model=model;
+//     this->year=year;
+// }
+
+// void Car::setCompany(string company){
+//     this->company=company;
+// }
+
+// void Car::setModel(string model){
+//     this->model=model;
+// }
+
+// void Car::setYear(int year){
+//     this->year=year;
+// }
+
+// ostream & operator<<(ostream &out, Car &c){
+//     out<<"Company: "<<c.company<<endl;
+//     out<<"Model: "<<c.model<<endl;
+//     out<<"Year: "<<c.year<<endl;
+
+//     return out;
+// }
+
+/*Write a C++ program to implement a class called BankAccount that has private member variables for account number and balance. Include member functions to deposit and withdraw money from the account.*/
+
 #include<iostream>
 
 using namespace std;
 
-class Car{
+class BankAccount{
     //data members
     private:
-    string company;
-    string model;
-    int year;
-
-    //member functions
-    public:
-    Car(string company="abc", string model="xyz",int year=0);
-    //accessor methods
-    string getCompany(){return company;}
-    string getModel(){return model;}
-    int getYear(){return year;}
-
-    //mutator methods
-    void setCompany(string company);
-    void setModel(string model);
-    void setYear(int year);
-    friend ostream & operator<<(ostream &out, Car &c);
+    int account_number;
+    float balance;
 };
 
 int main(){
-    Car c("Toyota","Innova",2009);
-    cout<<c;
+
     return 0;
 }
 
-Car::Car(string company, string model,int year){
-    this->company=company;
-    this->model=model;
-    this->year=year;
-}
 
-void Car::setCompany(string company){
-    this->company=company;
-}
-
-void Car::setModel(string model){
-    this->model=model;
-}
-
-void Car::setYear(int year){
-    this->year=year;
-}
-
-ostream & operator<<(ostream &out, Car &c){
-    out<<"Company: "<<c.company<<endl;
-    out<<"Model: "<<c.model<<endl;
-    out<<"Year: "<<c.year<<endl;
-
-    return out;
-}
