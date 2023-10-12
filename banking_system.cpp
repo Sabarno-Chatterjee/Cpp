059f69523f802b62dcb88852778f9a03bcabe0e2
Sabarno Chatterjee <sabarno.05@gmail.com>
2023-10-12 18:40:36 +0530

    banking system

---
+++ banking_system.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+
+using namespace std;
+
+class Account{
+
+};
+
+class Bank{
+
+};
+
+int main(){
+    int choice;
+    cout<<"Welcome to ABC Banking Services"<<endl;
+    cout<<"Which service would you like to access:"<<endl;
+    cout<<"1. Open new account"<<endl;
+    
+
+    return 0;
+}
