3b344b2c669ed80bccede1741596bf525396504c
Sabarno Chatterjee <sabarno.05@gmail.com>
2023-10-13 19:16:19 +0530

    edit

--- object_oriented_programming_questions.cpp
+++ object_oriented_programming_questions.cpp
@@ -360,6 +360,36 @@ class Date{
     int getYear() const{
         return year;
     }
+
+    bool isLeapYear() const{
+        if(year%4==0){
+            if(year%100){
+                if(year%400)
+                    return true;
+                else 
+                    return false;
+            }
+            else 
+                return true;
+        }
+        else
+            return false;
+    }
+
+    bool isValidDate() const {
+        if(month<1 || month>12)
+            return false;
+        
+        if(day<1 || day>31)
+            return false;
+
+        if((month==4 || month==6 || month==9 || month==11 )&&day>30)
+            return false;
+
+        if(month==2){
+
+        }
+    }
 };
 
 int main(){
