b91584517bf4c19a94fc7e60afa2a1e04dc57ffa
Sabarno Chatterjee <sabarno.05@gmail.com>
2023-10-14 09:19:21 +0530

    edit

--- file_handling.cpp
+++ file_handling.cpp
@@ -438,103 +438,125 @@ Class items should have:
 2. price
 3. quantity*/
 
-#include <iostream>
-#include <fstream>
-#include <cstring>
-#include <vector>
+// #include <iostream>
+// #include <fstream>
+// #include <cstring>
+// #include <vector>
 
-using namespace std;
+// using namespace std;
 
-class Item {
-public:
-    // Data members
-    string name;
-    float price;
-    int quantity;
+// class Item {
+// public:
+//     // Data members
+//     string name;
+//     float price;
+//     int quantity;
 
-    // Member functions
-    Item(string name = "abc", float price = 0, int quantity = 0); // Constructor
-    friend ofstream &operator<<(ofstream &outfile, Item &i);     // Friend function to write Item object to a file
-    friend ifstream &operator>>(ifstream &infile, Item &i);       // Friend function to read Item object from a file
-    friend ostream &operator<<(ostream &out, Item &i);            // Friend function to display Item object
-};
+//     // Member functions
+//     Item(string name = "abc", float price = 0, int quantity = 0); // Constructor
+//     friend ofstream &operator<<(ofstream &outfile, Item &i);     // Friend function to write Item object to a file
+//     friend ifstream &operator>>(ifstream &infile, Item &i);       // Friend function to read Item object from a file
+//     friend ostream &operator<<(ostream &out, Item &i);            // Friend function to display Item object
+// };
 
-// Constructor to initialize Item object
-Item::Item(string name, float price, int quantity) {
-    this->name = name;
-    this->price = price;
-    this->quantity = quantity;
-}
+// // Constructor to initialize Item object
+// Item::Item(string name, float price, int quantity) {
+//     this->name = name;
+//     this->price = price;
+//     this->quantity = quantity;
+// }
 
-// Friend function to write Item object to a file
-ofstream &operator<<(ofstream &outfile, Item &i) {
-    outfile << i.name << endl;
-    outfile << i.price << endl;
-    outfile << i.quantity << endl;
-    return outfile;
-}
+// // Friend function to write Item object to a file
+// ofstream &operator<<(ofstream &outfile, Item &i) {
+//     outfile << i.name << endl;
+//     outfile << i.price << endl;
+//     outfile << i.quantity << endl;
+//     return outfile;
+// }
 
-// Friend function to display Item object
-ostream &operator<<(ostream &out, Item &i) {
-    out << "Name: " << i.name << endl;
-    out << "Price: " << i.price << endl;
-    out << "Quantity: " << i.quantity << endl;
-    return out;
-}
+// // Friend function to display Item object
+// ostream &operator<<(ostream &out, Item &i) {
+//     out << "Name: " << i.name << endl;
+//     out << "Price: " << i.price << endl;
+//     out << "Quantity: " << i.quantity << endl;
+//     return out;
+// }
 
-// Friend function to read Item object from a file
-ifstream &operator>>(ifstream &infile, Item &i) {
-    infile >> i.name;
-    infile >> i.price;
-    infile >> i.quantity;
-    return infile;
-}
+// // Friend function to read Item object from a file
+// ifstream &operator>>(ifstream &infile, Item &i) {
+//     infile >> i.name;
+//     infile >> i.price;
+//     infile >> i.quantity;
+//     return infile;
+// }
 
-int main() {
-    int quantity, n;
-    string name;
-    float price;
+// int main() {
+//     int quantity, n;
+//     string name;
+//     float price;
 
-    cout << "Enter the number of items" << endl;
-    cin >> n;
+//     cout << "Enter the number of items" << endl;
+//     cin >> n;
 
-    vector<Item*> list; // Create a vector to store pointers to Item objects
+//     vector<Item*> list; // Create a vector to store pointers to Item objects
 
-    for (int i = 0; i < n; i++) {
-        cout << "Enter the data for item " << i + 1 << ":" << endl;
-        cin >> name;
-        cin >> price;
-        cin >> quantity;
-        list.push_back(new Item(name, price, quantity)); // Create and add Item objects to the vector
-    }
+//     for (int i = 0; i < n; i++) {
+//         cout << "Enter the data for item " << i + 1 << ":" << endl;
+//         cin >> name;
+//         cin >> price;
+//         cin >> quantity;
+//         list.push_back(new Item(name, price, quantity)); // Create and add Item objects to the vector
+//     }
 
-    ofstream outfile("item.txt"); // Open the output file
+//     ofstream outfile("item.txt"); // Open the output file
 
-    for (vector<Item*>::iterator itr = list.begin(); itr != list.end(); itr++) {
-        outfile << **itr; // Write Item objects to the file
-    }
+//     for (vector<Item*>::iterator itr = list.begin(); itr != list.end(); itr++) {
+//         outfile << **itr; // Write Item objects to the file
+//     }
 
-    outfile.close(); // Close the output file
+//     outfile.close(); // Close the output file
 
-    Item item;
-    ifstream infile;
-    infile.open("item.txt");
-    if (!infile)
-        cout << "File does not exist" << endl;
+//     Item item;
+//     ifstream infile;
+//     infile.open("item.txt");
+//     if (!infile)
+//         cout << "File does not exist" << endl;
 
-    for (int i = 0; i < n; i++) {
-        infile >> item; // Read Item objects from the file
-        cout << "Item " << i + 1 << ":" << endl;
-        cout << item << endl; // Display Item objects
-    }
+//     for (int i = 0; i < n; i++) {
+//         infile >> item; // Read Item objects from the file
+//         cout << "Item " << i + 1 << ":" << endl;
+//         cout << item << endl; // Display Item objects
+//     }
 
-    if (infile.eof())
-        infile.close(); // Close the input file
+//     if (infile.eof())
+//         infile.close(); // Close the input file
 
-    // Deallocate memory by deleting the Item objects
-    for (vector<Item*>::iterator itr = list.begin(); itr != list.end(); itr++) {
-        delete *itr;
-    }
+//     // Deallocate memory by deleting the Item objects
+//     for (vector<Item*>::iterator itr = list.begin(); itr != list.end(); itr++) {
+//         delete *itr;
+//     }
+
+//     return 0;
+// }
+
+
+
+/*Write a C++ program to create a new text file and write some text into it.*/
+
+
+#include<iostream>
+#include<fstream>
+#include<cstring>
+
+int main(){
+    std::string text;
+    std::cout<<"Enter some text"<<std::endl;
+    std::getline(std::cin,text);
+    
+    std::ofstream outfile("file_handling.txt");
+    outfile<<text;
+    
+    outfile.close();
 
     return 0;
 }
