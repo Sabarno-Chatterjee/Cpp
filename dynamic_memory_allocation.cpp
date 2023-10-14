f7b28d86bcee018cbe83168572207c11aee37634
Sabarno Chatterjee <sabarno.05@gmail.com>
2023-10-14 16:24:00 +0530

    dynamic memory allocation array

--- dynamic_memory_allocation.cpp
+++ dynamic_memory_allocation.cpp
@@ -1,27 +1,56 @@
 /*Write a C++ program to dynamically allocate an integer, a character and a string and assign a value to them.*/
 
+// #include<iostream>
+// #include<cstring>
+
+// int main(){
+
+//     int *Iptr=new (int);
+//     *Iptr=21;
+
+//     char *Cptr=new char;
+//     *Cptr='J';
+
+//     std::string *Sptr=new std::string;
+//     *Sptr = "Dodo";
+
+//     std::cout<<"Dynamically allocated integer: "<<*Iptr<<std::endl;
+//     std::cout<<"Dynamically allocated character: "<<*Cptr<<std::endl;
+//     std::cout<<"Dynamically allocated string: "<<*Sptr<<std::endl;
+
+//     //Deallocate memory:
+//     delete Iptr;
+//     delete Cptr;
+//     delete Sptr;
+
+//     return 0;
+// }
+
+
+/*Write a C++ program to dynamically allocate an array of integers and strings and initialize its elements.*/
+
+
 #include<iostream>
 #include<cstring>
 
 int main(){
+    int size=5;
+    int *IntegerArray= new int[size];
 
-    int *Iptr=new (int);
-    *Iptr=21;
+    std::string *StringArray=new std::string[size];
 
-    char *Cptr=new char;
-    *Cptr='J';
+    for(int i=0; i<size; i++){
+        IntegerArray[i]=i+1;
+        StringArray[i]="Element_"+std::to_string(i+1);
+    }
 
-    std::string *Sptr=new std::string;
-    *Sptr = "Dodo";
+    for(int i=0; i<size;i++){
+        std::cout<<IntegerArray[i]<<" "<<StringArray[i]<<std::endl;
+    }
 
-    std::cout<<"Dynamically allocated integer: "<<*Iptr<<std::endl;
-    std::cout<<"Dynamically allocated character: "<<*Cptr<<std::endl;
-    std::cout<<"Dynamically allocated string: "<<*Sptr<<std::endl;
-
-    //Deallocate memory:
-    delete Iptr;
-    delete Cptr;
-    delete Sptr;
+    //deallocation of memory
+    delete []IntegerArray;
+    delete []StringArray;
 
     return 0;
 }
