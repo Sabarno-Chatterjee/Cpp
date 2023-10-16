b326cd6381374ac0d55125685d8e777fb507c09d
Sabarno Chatterjee <sabarno.05@gmail.com>
2023-10-16 08:18:32 +0530

    edit

--- banking_system.cpp
+++ banking_system.cpp
@@ -1,6 +1,5 @@
 #include<iostream>
 
-using namespace std;
 
 class Account{
 
@@ -15,6 +14,10 @@ int main(){
     cout<<"Welcome to ABC Banking Services"<<endl;
     cout<<"Which service would you like to access:"<<endl;
     cout<<"1. Open new account"<<endl;
+    cout<<"2. Deposit"<<endl;
+    cout<<"3. Withdraw"<<endl;
+    cout<<"4. Balance enquiry"<<endl;
+    cout<<"5. Exit"<<endl;
     
 
     return 0;
